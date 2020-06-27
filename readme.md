e096132428db620fd0aed11c499bd1803884214b
Fabian Krutsch <f.krutsch@gmx.de>
2020-06-27 12:16:43 +0100

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,41 @@
+# Progressive Pictures
+
+> This library enhances Picture Elements with Progressive Image Loading and thus improves the Intial time of Images being displayed.
+
+## Install via NPM
+
+```
+$ npm install progressive-picture
+```
+
+## Install via Script Tag
+
+```
+<script defer type="module" src="https://unpkg.com/progressive-picture"></script>
+```
+
+## Example
+
+```html
+<picture>
+  <source srcset="eu.preview.webp" data-src="eu.webp" type="image/webp" />
+  <source srcset="eu.preview.jpg" data-src="eu.jpg" type="image/jpg" />
+  <img
+    src="eu.preview.jpg"
+    data-src="eu.jpg"
+    loading="lazy"
+    width="500"
+    height="750"
+    data-alt="Logo"
+  />
+</picture>
+```
+
+## Usage
+
+The `srcset`s of `<source>` and the `src` of `<img>` has to be filled with the URI for the low-quality Image. The data-src holds the high-quality Image and will be replaced once it has been loaded.  
+Also, there is a data-alt Attribute that can be applied as alt, once the Image was replaced. This fixes the inelegance of displaying text before an Image appears.
+
+### Further
+
+As shown in the Example, this library works great in combination with native lazy loading. Tip: add width and height to fix layout shifts.
---
+++ src/progressive-image.ts
@@ -0,0 +1,96 @@
+const css = document.createElement("style");
+css.type = "text/css";
+css.innerText =
+  "picture { display: inline-block; } picture > img { filter: blur(0px); transition: all 0.7s ease-in; } picture > img[data-src] { filter: blur(2px); }";
+document.head.appendChild(css);
+
+const progessiveLoaded = new WeakSet<HTMLPictureElement>();
+const observer = new IntersectionObserver(
+  (entries) => {
+    for (const entry of entries) {
+      if (
+        entry.intersectionRatio > 0 &&
+        !progessiveLoaded.has(entry.target as HTMLPictureElement)
+      ) {
+        const sources = entry.target.querySelectorAll("source");
+        const img = entry.target.querySelector("img");
+        if (!img) break;
+
+        preload(sources, "srcset", img, false).then((preloadedSource) => {
+          if (preloadedSource) {
+            progessiveLoaded.add(entry.target as HTMLPictureElement);
+            return; // source element is being used -> no need to preload Image in <img>
+          }
+
+          preload([img], "src", img, true).then(
+            (preloadedImage) =>
+              preloadedImage &&
+              progessiveLoaded.add(entry.target as HTMLPictureElement)
+          );
+        });
+      }
+    }
+  },
+  { rootMargin: "0px", threshold: 0.5 }
+);
+
+const DOMObserver = new MutationObserver((entries) => {
+  for (const entry of entries) {
+    for (const node of entry.addedNodes) {
+      if (node instanceof HTMLPictureElement) {
+        observer.observe(node);
+      }
+    }
+    for (const node of entry.removedNodes) {
+      if (node instanceof HTMLPictureElement) {
+        observer.unobserve(node);
+        progessiveLoaded.delete(node);
+      }
+    }
+  }
+}).observe(document.body, { childList: true, subtree: true });
+
+const pictures = document.querySelectorAll("picture");
+for (const picture of pictures) {
+  observer.observe(picture);
+}
+
+async function preload(
+  elements: Array<HTMLImageElement> | NodeListOf<HTMLSourceElement>,
+  source: string,
+  original: HTMLImageElement,
+  alt: boolean
+) {
+  for (const img of elements) {
+    const preload = new Image();
+    if (!original.currentSrc) {
+      await {
+        then: (resolve: (_: HTMLImageElement) => Promise<HTMLImageElement>) =>
+          (original.onload = () => resolve(original)),
+      };
+    }
+    const current = original.currentSrc.split("/").slice(-1)[0];
+    const sourceSrc = img.getAttribute(source)?.split("/").slice(-1)[0];
+
+    // preload when data-src for img and match-media for source
+    if (img.dataset.src && current === sourceSrc) {
+      preload.src = img.dataset.src;
+      original.removeAttribute("data-src");
+      elements.forEach((elem: HTMLSourceElement | HTMLImageElement) =>
+        elem.removeAttribute("data-src")
+      );
+
+      await {
+        then: (resolve: (_: HTMLImageElement) => Promise<HTMLImageElement>) =>
+          (preload.onload = () => resolve(preload)),
+      };
+      img.setAttribute(source, preload.src);
+
+      if (alt && img.dataset.alt) {
+        img.setAttribute("alt", img.dataset.alt);
+        img.removeAttribute("data-alt");
+      }
+      return true;
+    }
+  }
+}
